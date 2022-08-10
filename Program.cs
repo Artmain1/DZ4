b3f82aa9caf1585c8b4b4b622f6117adf6f7c8d6
Artur <89503357000@ya.ru>
2022-08-10 11:10:10 +0600

    main

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи 1 число: ");
+int n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи 2 число: ");
+int m = Convert.ToInt16(Console.ReadLine ());
+for (int i = n; i <= m; i++)
+{
+if (i % 2 == 0) 
+    {
+    Console.Write(i + " ");
+    }
+}
+        Console.WriteLine();
